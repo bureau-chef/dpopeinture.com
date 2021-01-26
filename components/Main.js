5b8e0a3f3026239c62ffe12c99fe8b71b134cd1b
bureau-chef <mike@bureau-chef.ca>
2021-01-25 21:29:13 -0500

    img

--- components/Main.js
+++ components/Main.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Fragment } from 'react'
 import Image from 'next/image'
 
 import { Row, Col } from 'react-flexbox-grid/dist/react-flexbox-grid'
@@ -10,6 +11,107 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger)
 
 function Main(props) {
+
+    const imageContent = {
+        img : [
+            {
+                'src': '/photos/IMG_5776.JPEG',
+                'alt': 'DPO Peinture Inc. - Teinture - Escalier',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/A733804F-69EC-4E7F-A75A-4E9AE79CB6BB.JPEG',
+                'alt': 'DPO Peinture Inc. - Teinture - Patio - Extérieur',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/IMG_5784-c.jpg',
+                'big': '/photos/IMG_5784.JPEG',
+                'alt': 'DPO Peinture Inc. - Teinture - Patio - Extérieur',
+                'w': 716,
+                'h': 716,
+                'bigw': 716,
+                'bigh': 960,
+            },
+            {
+                'src': '/photos/IMG_5772.JPG',
+                'alt': 'DPO Peinture Inc. - Peinture - Intérieur',
+                'w': 1200,
+                'h': 1200,
+            },
+            {
+                'src': '/photos/IMG_5783.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Revêtement - Extérieur',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/131EBC52-B061-45D1-BA01-AB0AFCEEF7BB.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Intérieur',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/IMG_5779.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Teinture - Extérieur',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/EE6EB1FA-61A6-4C14-BA32-A99DC7D94D05.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Extérieur',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/IMG_5773.JPG',
+                'alt': 'DPO Peinture Inc. - Peinture - Intérieur - Escalier',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/16F85998-9ADF-450D-A735-2010446F9E2E.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Extérieur - Porte de garage',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/IMG_5774.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Intérieur - Armoire de cuisine',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/IMG_5780-c.jpg',
+                'big': '/photos/IMG_5780.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Intérieur - Commercial',
+                'w': 1536,
+                'h': 1536,
+                'bigw': 2048,
+                'bigh': 1536,
+            },
+            {
+                'src': '/photos/IMG_5775.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Intérieur - Brique',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/8ECEE6A5-F70C-41FC-AFED-D07125FA9512.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Extérieur - Brique',
+                'w': 2048,
+                'h': 2048,
+            },
+            {
+                'src': '/photos/IMG_5679.JPEG',
+                'alt': 'DPO Peinture Inc. - Peinture - Extérieur - Brique',
+                'w': 2048,
+                'h': 2048,
+            }
+        ]
+    }
 
     /* const router = useRouter() */
 
@@ -31,32 +133,15 @@ function Main(props) {
             autoAlpha: 0,
             ease:Power3.easeInOut,
             duration: 1.5
-        })
-        gsap.from('#Home-title', {
-            autoAlpha: 0,
-            ease:Power3.easeInOut,
-            delay:.65,
-            duration: .5
-        })
-        gsap.from('#Language', {
-            x: '-5px', 
-            autoAlpha: 0,
-            ease:Power3.easeInOut,
-            delay:.85,
-            duration: .5
-        })
-        gsap.from('.scroll-anim-cont', {
-            y: '-5px', 
-            opacity: 0,
-            ease:Power3.easeInOut,
-            delay:.95,
-            duration: .5,
-            onComplete: scrollAnimTrigger
-        }) */
+        })*/
     }, [])
 
     return (
         <main id="Content" className="section">
+
+            <div className="circle top"></div>
+            <div className="circle bottom"></div>
+
             <div className="wrapper">
                 <Row middle="xs">
                     <Col sm={12} md={7}>
@@ -99,6 +184,9 @@ function Main(props) {
                                 </p>
                             </Col>
                         </Row>
+                        <Row>
+                            <hr className="spacer" />
+                        </Row>
                         
                     </Col>
                     <Col sm={12} md={4} smOffset={1}>
@@ -118,8 +206,28 @@ function Main(props) {
                         </div>
                     </Col>
                 </Row>
+
+                <Row className="mosaic">
+
+                    {imageContent.img.map((IMG, index) => (
+                        <Fragment>
+                            <Col sm={12} md={2} className='img-thumbnail' >
+                                <Image
+                                    src={IMG.src}
+                                    alt={IMG.alt}
+                                    width={IMG.w}
+                                    height={IMG.h}
+                                    big={IMG.big}
+                                    bigw={IMG.bigw}
+                                    bigh={IMG.bigh}
+                                />
+                            </Col>
+                       </Fragment>
+                    )
+                    )}
+
+                </Row>
             </div>
-            
             
         </main>
     )
